3545d9bf711a7ee0bbcab006f06d353a4fa4cb7f
Geoff Boeing <boeing@usc.edu>
2021-01-07 09:13:06 -0800

    user value closer to validation value

--- worldwide/02-attach-elevation/05-process-elevations.py
+++ worldwide/02-attach-elevation/05-process-elevations.py
@@ -6,6 +6,7 @@
 
 import geopandas as gpd
 import json
+import numpy as np
 import osmnx as ox
 import pandas as pd
 
@@ -24,21 +25,21 @@ with open('../config.json') as f:
 
 
 aster = pd.read_csv(config['elevation_aster_elevations_path']).set_index('osmid').sort_index()
-print(ox.ts(), 'loaded aster elevation data for', len(aster), 'nodes')
+print(ox.ts(), 'loaded ASTER elevation data for', len(aster), 'nodes')
 
 
 # In[ ]:
 
 
 srtm = pd.read_csv(config['elevation_srtm_elevations_path']).set_index('osmid').sort_index()
-print(ox.ts(), 'loaded srtm elevation data for', len(srtm), 'nodes')
+print(ox.ts(), 'loaded SRTM elevation data for', len(srtm), 'nodes')
 
 
 # In[ ]:
 
 
 google = pd.read_csv(config['elevation_google_elevations_path']).set_index('osmid').sort_index()
-print(ox.ts(), 'loaded google elevation data for', len(google), 'nodes')
+print(ox.ts(), 'loaded Google elevation data for', len(google), 'nodes')
 
 
 # In[ ]:
@@ -72,55 +73,42 @@ df = df.reindex(columns=cols)
 
 
 # no row is null in both aster and srtm values
-print(ox.ts(), pd.isnull(df['elev_aster']).sum(), 'null aster values')
-print(ox.ts(), pd.isnull(df['elev_srtm']).sum(), 'null srtm values')
-print(ox.ts(), (pd.isnull(df['elev_aster']) & pd.isnull(df['elev_srtm'])).sum(), 'nodes are null in both aster and srtm')
+print(ox.ts(), pd.isnull(df['elev_aster']).sum(), 'null ASTER values')
+print(ox.ts(), pd.isnull(df['elev_srtm']).sum(), 'null SRTM values')
+print(ox.ts(), (pd.isnull(df['elev_aster']) & pd.isnull(df['elev_srtm'])).sum(), 'nodes are null in both ASTER and SRTM')
 
 
 # In[ ]:
 
 
 # calculate differences in ASTER, SRTM, and Google elevation values
-df['elev_diff_aster_srtm'] = df['elev_aster'] - df['elev_srtm']
-df['elev_diff_aster_google'] = df['elev_aster'] - df['elev_google']
-df['elev_diff_srtm_google'] = df['elev_srtm'] - df['elev_google']
-
+df['elev_diff_aster_google'] = (df['elev_aster'] - df['elev_google']).fillna(np.inf)
+df['elev_diff_srtm_google'] = (df['elev_srtm'] - df['elev_google']).fillna(np.inf)
 
 # In[ ]:
 
 
-# by default use ASTER values as elevation, but fill nulls with SRTM values
-df['elevation'] = df['elev_aster'].fillna(df['elev_srtm']).astype(int)
-cols = df.columns.tolist()[-1:] + df.columns.tolist()[:-1]
-df = df[cols]
+# in each row identify if SRTM or ASTER has smaller absolute difference from Google's value
+use_srtm = df['elev_diff_srtm_google'].abs() <= df['elev_diff_aster_google'].abs()
+pct = 100 * use_srtm.sum() / len(df)
+print(ox.ts(), f'{pct:0.1f}% of nodes will use SRTM values, the rest will use ASTER')
 
 
-# In[ ]:
 
+# assign elevation as the SRTM or ASTER value closer to Google's, as a tie-breaker
+df['elevation'] = np.nan
+df.loc[use_srtm, 'elevation'] = df.loc[use_srtm, 'elev_srtm']
+df.loc[~use_srtm, 'elevation'] = df.loc[~use_srtm, 'elev_aster']
 
-# if ASTER and SRTM differ by more than SRTM spec's error (16 meters),
-# use whichever is closer to the Google (validation) value, as tie-breaker
-srtm_error = 16
-mask1 = df['elev_diff_aster_srtm'].abs() > srtm_error
-mask2 = df['elev_diff_aster_google'].abs() > df['elev_diff_srtm_google'].abs()
-df_bad = df[mask1 & mask2]
-
-# what % of ASTER values will we replace with SRTM values?
-pct = 100 * len(df_bad) / len(df)
-print(ox.ts(), f'replace {pct:0.2f}% of rows\' bad aster values with srtm values')
-
-# replace these ASTER values with SRTM values and ensure all elevations are non-null
-labels = set(df_bad.index.sort_values())
-df.loc[labels, 'elevation'] = df['elev_srtm']
+# ensure all elevations are non-null
 assert pd.notnull(df['elevation']).all()
-
 df['elevation'] = df['elevation'].astype(int)
 
 
 # In[ ]:
 
 
-# save elevations to disk for subsequent attaching to graph file's nodes
+# save elevations to disk for subsequently attaching to graph file's nodes
 cols = ['elevation', 'elev_aster', 'elev_srtm', 'elev_google']
 df[cols].to_csv(config['elevation_final_path'], index=True, encoding='utf-8')
 print(ox.ts(), 'saved', len(df), 'elevations to disk at', config['elevation_final_path'])
